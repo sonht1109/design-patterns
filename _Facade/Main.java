e4c0c4e158650d1d702914414a172e8d6fa1e9ef
sonht <sonht1109@gmail.com>
2022-06-07 17:03:43 +0700

    [master] update: facade concept

--- _Facade/Main.java
+++ _Facade/Main.java
@@ -1,5 +1,9 @@
 package _Facade;
 
+/**
+ * cung cấp giao diện đơn giản cho một hệ thống con để giao tiếp với client để che giấu sự phức tạp của hệ thống
+ */
+
 public class Main {
   public static void main(String[] args) {
     ComputerFacade computerFacade = new ComputerFacade();
